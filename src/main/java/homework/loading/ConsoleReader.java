b0b6aea4308234635aa51321bd1a03af1629e259
Antonin Danek <danek@antonindanek.cz>
2020-02-01 13:18:34 +0100

    Add console loader

---
+++ src/main/java/homework/loading/ConsoleReader.java
@@ -0,0 +1,29 @@
+package homework.loading;
+
+import java.util.Scanner;
+
+import homework.data.Storage;
+
+public class ConsoleReader extends InputReader {
+
+    public ConsoleReader(Storage storage) {
+    	super(storage);
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println(message);
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            while (sc.hasNextLine()) {
+                readAndStoreLine(sc);
+            }
+        }
+
+    }
+
+
+
+}
